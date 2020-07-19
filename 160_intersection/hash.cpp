d99f4ffcf9ac2b715ebc4506f577d7f38ef77ea4
YuxuanCheng <yuxuancheng@outlook.com>
2020-07-19 15:42:22 -0400

    Added hash in cpp

---
+++ 160_intersection/hash.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_set>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode * && getIntersectionNode(ListNode * & headA, ListNode * & headB) {
+        std::unordered_set<ListNode *> node_set;
+        ListNode* pointer_A = headA;
+        ListNode* pointer_B = headB;
+        while(pointer_A != NULL){
+            node_set.emplace(pointer_A);
+            pointer_A = pointer_A->next;
+        }
+        while(pointer_B != NULL){
+            if (node_set.find(pointer_B)!= node_set.end())
+                return std::move(pointer_B);
+            else
+                pointer_B = pointer_B->next;
+        }
+        return std::move(pointer_B);
+    }
+};
